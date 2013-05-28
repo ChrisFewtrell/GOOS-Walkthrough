db8a40e8dbd1127b3346caef57fdbadcbaf8fbdc
unknown <thomas.oshea@interview.GrantaDesign.local>
2013-05-28 10:01:12 +0100

    Aisling's interview attempt at the Checkout problem

--- Checkout/Checkout.Tests/CheckoutTests.cs
+++ Checkout/Checkout.Tests/CheckoutTests.cs
@@ -14,9 +14,56 @@ namespace Checkout.Tests
         {
             var checkout = new Checkout();
 
+
             var totalPrice = checkout.GetTotal(string.Empty);
 
             Assert.That(totalPrice, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void GetTotal_ShouldReturn50_WhenItemIsScanned()
+        {
+
+            var checkout = new Checkout();
+
+
+            var scannedItemPrice = checkout.GetTotal("A");
+
+            Assert.That(scannedItemPrice, Is.EqualTo(50));
+        
+        }
+
+        [Test]
+        public void GetTotal_ShouldReturn80_WhenItemScanned()
+        {
+               var checkout = new Checkout();
+
+
+            var scannedItemPrice = checkout.GetTotal("AB");
+
+            Assert.That(scannedItemPrice, Is.EqualTo(80));
+        }
+
+        [Test]
+        public void GetTotal_ShouldReturn115_WhenItemScanned()
+        {
+            var checkout = new Checkout();
+
+
+            var scannedItemPrice = checkout.GetTotal("ABCD");
+
+            Assert.That(scannedItemPrice, Is.EqualTo(115));
+        }
+
+        [Test]
+        public void GetTotal_ShouldReturn115_WhenBadItemScanned()
+        {
+            var checkout = new Checkout();
+
+
+            var scannedItemPrice = checkout.GetTotal("ABCDE");
+
+            Assert.That(scannedItemPrice, Is.EqualTo(115));
         }
     }
 }
--- Checkout/Checkout/Checkout.cs
+++ Checkout/Checkout/Checkout.cs
@@ -7,9 +7,36 @@ namespace Checkout
 {
     public class Checkout
     {
-        public int GetTotal(string skus)
+        Dictionary<char, double> priceList;
+        public Checkout()
         {
-            throw new NotImplementedException();
+            priceList = new Dictionary<char, double>();
+            priceList.Add('A', 50);
+            priceList.Add('B', 30);
+            priceList.Add('C', 20);
+            priceList.Add('D', 15);
+        }
+        public double GetTotal(string skus)
+        {
+            double result;
+            double total = 0;
+            int length = skus.Length;
+            for (int i = 0; i < length; i++)
+            {
+                if(priceList.TryGetValue(skus[i],out result))
+                    total += result;
+            
+                /*if (skus[i] == 'A')
+                    total += 50;
+                else if (skus[i] == 'B')
+                    total += 30;
+                else if (skus[i] == 'C')
+                    total += 20;
+                else if (skus[i] == 'D')
+                    total += 15;*/
+            }
+            //throw new NotImplementedException();
+            return total;
         }
     }
 }
